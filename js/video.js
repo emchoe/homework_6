f7312aa005abc8cc21945e0ddcd6ffcba7aba1b4
emchoe <70600854+emchoe@users.noreply.github.com>
2020-11-17 15:47:56 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -7,7 +7,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
-	volume.innerHTML = video.volume*100 + "%"
+	volume.innerHTML = video.volume*100 + "%";
 });
 
 
